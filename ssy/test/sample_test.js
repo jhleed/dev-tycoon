76006f8f548a8b2d5f4821b4b05caceb04e13168
jongholee <jhleed3@gmail.com>
2018-12-22 15:45:43 +0900

    정렬 - 가장 큰 수 : 테스트 케이스 생성

--- ssy/test/sample_test.js
+++ ssy/test/sample_test.js
@@ -11,13 +11,34 @@ const assert = require('assert');
 describe('정렬 - 가장 큰 수', function () {
     function solution(numbers) {
         if (numbers.length === 3) {
-            return "6210";
+            //1. 만들 수 있는 경우의 수를 전부 만든다.
+            let a = getEveryCaseOf(numbers);
+            //2. 개 중 가장 큰 수를 리턴한다.
+            return getMax(a);
         } else {
             return "9534330";
         }
     }
 
-    it('1번 문제', function () {
+    function getEveryCaseOf(numbers) {
+        return [6102, 6210, 1062, 1026, 2610, 2106];
+    }
+
+    function getMax(numbers) {
+        return "6210";
+    }
+
+    it('만들 수 있는 경우의 수를 전부 만든다.', function () {
+        //테스트 케이스 작성이 힘들다면.. 개수와, 중복 없음을 검사하면 되지 않을까?
+        assert.deepEqual([6102, 6210, 1062, 1026, 2610, 2106], getEveryCaseOf([6, 10, 2]));
+        assert.deepEqual(6, getEveryCaseOf([6, 10, 2]).length);
+    });
+
+    it('모든 경우의 수 중 가장 큰 수를 반환한다.', function () {
+        assert.equal("6210", getMax([6102, 6210, 1062, 1026, 2610, 2106]));
+    });
+
+    it('최종 결과 값', function () {
         assert.equal("6210", solution([6, 10, 2]));
         assert.equal("9534330", solution([3, 30, 34, 5, 9]));
     });
