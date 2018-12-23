43b620b2bf798a8779be328a18be14f00ebdd53d
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2018-12-23 16:40:37 +0900

    짝수와 홀수:

---
+++ ssy/test/EvenorOdd.js
@@ -0,0 +1,17 @@
+const assert = require('assert');
+
+describe('홀수와 짝수', function () {
+    function solution(num) {
+        var answer = '';
+
+        num % 2 == 0 ? answer = "Even" : answer = "Odd";
+
+        return answer;
+    }
+
+
+it('맞음', function () {
+    assert.equal(solution(5), "Odd");
+});
+
+});
