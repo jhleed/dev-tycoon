cdd11cca50c088dee97e0d53cb708a9aa8cf8d02
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2019-01-21 19:26:32 +0900

    Palindrome 판별

--- ssy/test/Palindrome.js
+++ ssy/test/Palindrome.js
@@ -2,35 +2,62 @@ const assert = require('assert');
 
 describe('Palindrome 판별', function () {
     function solution(word) {
-        // 단어를 반으로 쪼갠 값을 middle에 넣는다. (word.length()/2)
-        let middle = parseInt(word.length/2);
 
-        // 0번째 ~ (word.length()/2)-1번째를 front에,  (word.length()/2)+1 ~ word.length()를 back에 넣는다.
-        // 홀수일 때에는 middle+1, 짝수일 때에는 middle
+        // 홀수와 짝수를 판별한다.
+
+        if (word.length % 2 !== 0) {
+
+            let middle = parseInt(word.length / 2);
+
+            // 0번째 ~ (word.length()/2)-1번째를 front에,  (word.length()/2)+1 ~ word.length()를 back에 넣는다.
+            // 홀수일 때에는 middle+1, 짝수일 때에는 middle
+            let front = word.substring(0, middle);
+            let back = word.substring(middle + 1, word.length);
+
+            // back을 reverse한다 (문자열 뒤집기? -> 이건 검색해보기)
+            back = back.split("").reverse().join("");
+
+            // front === back이면 Palindrome, 아니면 Not Palindrome
+
+
+            if (front === back) {
+                return "Palindrome";
+            }
+            return "Not Palindrome"
+
+        }
+
+
+        // word/2
+
+        let middle = word.length / 2;
+
+        // front = 0~middle-1
+        // back = middle~length
+
         let front = word.substring(0, middle);
-        let back = word.substring(middle+1, word.length);
+        let back = word.substring(middle, word.length);
+        // front === back reverse ?
 
-        // back을 reverse한다 (문자열 뒤집기? -> 이건 검색해보기)
-        back = back.split("").reverse().join("");
-
-        // front === back이면 Palindrome, 아니면 Not Palindrome
-
-
-        if(front === back){
+        if (front === back.split("").reverse().join("")) {
             return "Palindrome";
         }
-        return "Not Palindrome"
+
+        return "Not Palindrome";
+
     }
-
-
 
 
     it('짝수일 때 - Palindrome', function () {
         assert.equal(solution('mammam'), "Palindrome");
+        assert.equal(solution('mm'), "Palindrome");
+        assert.equal(solution('qwerttrewq'), "Palindrome");
     });
 
     it('짝수일 때 - Not Palindrome', function () {
         assert.equal(solution('mamabmam'), "Not Palindrome");
+        assert.equal(solution('ab'), "Not Palindrome");
+        assert.equal(solution('iwantkfc'), "Not Palindrome");
     });
 
     it('홀수일 때 - Palindrome', function () {
