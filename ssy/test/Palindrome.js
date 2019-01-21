e933fd494a5fa2546d40114ce2b474d9758149d6
Sunyoung Shin <s2017i30@e-mirim.hs.kr>
2019-01-21 19:53:00 +0900

    Palindrome 판별 (리팩토링)

--- ssy/test/Palindrome.js
+++ ssy/test/Palindrome.js
@@ -2,49 +2,22 @@ const assert = require('assert');
 
 describe('Palindrome 판별', function () {
     function solution(word) {
-
-        // 홀수와 짝수를 판별한다.
-
-        if (word.length % 2 !== 0) {
-
-            let middle = parseInt(word.length / 2);
-
-            // 0번째 ~ (word.length()/2)-1번째를 front에,  (word.length()/2)+1 ~ word.length()를 back에 넣는다.
-            // 홀수일 때에는 middle+1, 짝수일 때에는 middle
-            let front = word.substring(0, middle);
-            let back = word.substring(middle + 1, word.length);
-
-            // back을 reverse한다 (문자열 뒤집기? -> 이건 검색해보기)
-            back = back.split("").reverse().join("");
-
-            // front === back이면 Palindrome, 아니면 Not Palindrome
-
-
-            if (front === back) {
-                return "Palindrome";
-            }
-            return "Not Palindrome"
-
-        }
-
-
-        // word/2
-
-        let middle = word.length / 2;
-
-        // front = 0~middle-1
-        // back = middle~length
-
+        let middle = parseInt(word.length / 2);
         let front = word.substring(0, middle);
-        let back = word.substring(middle, word.length);
-        // front === back reverse ?
+        let back;
 
+        // 짝수와 홀수를 판별해서 자르는 값을 나눔
+        if (word.length % 2 !== 0) {
+            back = word.substring(middle + 1, word.length);
+        }
+        else{
+            back = word.substring(middle, word.length);
+        }
+        // front와 뒤집어진 back을 비교함
         if (front === back.split("").reverse().join("")) {
             return "Palindrome";
         }
-
         return "Not Palindrome";
-
     }
 
 
